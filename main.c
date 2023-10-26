2977088ab6dfe14f5aee2559ad29953660275525
Deadman621 <affan.basit116@gmail.com>
2023-10-26 21:13:32 +0500

    Included Library

---
+++ main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+#include<math.h>
+#include "CGLib/src/cglib.h"
+#include "CGLib/src/cglib-other.h"
+
+int main(void) {
+
+    printf("Hello World");
+
+    return 0;
+}
--- prototype.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main() {
-    printf("CALCULATE YOUR CARBON EMISSIONS");
-    
-    
-    return 0;
-}
