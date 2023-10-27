116726b33018356d4327af1bf58e760d5f18ec06
Deadman621 <affan.basit116@gmail.com>
2023-10-27 23:39:15 +0500

    Inputs Completed Except for Food

--- input.c
+++ input.c
@@ -3,16 +3,39 @@
 #include<string.h>
 #include<time.h>
 #include<math.h>
-#include"CGLib/src/cglib.h"
-#include"CGLib/src/cglib-other.h"
 
 int SumArray(int n, int arr[]) {
-
     int sum;
     for(int i = 0; i < n; i++) 
         sum += arr[i];
 
     return sum;
+}
+
+int DataValidationInteger(char str[], int  variable) {
+    int valid;
+    valid = scanf("%d", &variable);
+    if (valid != 1) {
+        while(valid != 1) {
+            while ((getchar()) != '\n');
+            printf("Invalid Input! Renter Value for %s: ", str);
+            valid = scanf("%d", &variable);
+        }
+    }
+    return variable;
+}
+
+int yes_no(char variable) {
+    if (variable == 'y') 
+        return 1;
+    else if (variable == 'n')
+        return 0;
+    else {
+        while ((getchar()) != '\n');
+        printf("Invalid Input! Please Answer in (y/n): ");
+        scanf("%c", &variable);
+        yes_no(variable);
+    }
 }
 
 void EnergyInput(int energy_type[]) {
@@ -29,3 +52,43 @@ void EnergyInput(int energy_type[]) {
     }
 }
 
+int VehicleIO(int arr[][2], int product[], int number_of_vehicles) {
+
+    if (number_of_vehicles == 0) 
+        return 0;
+    else if (number_of_vehicles == 1) {
+        printf("Enter The Consumption of Fuel Per Galon For Your Vehicle: ");
+        arr[0][0] = DataValidationInteger("Consumption of Fuel Per Galon", arr[0][0]);
+        printf("Enter The Miles Drived in an 12 Months for your Vehicle: ");
+        arr[0][1] = DataValidationInteger("Miles Driven in an 12 Months for your Vehicle", arr[0][1]);
+    }
+    else {
+        for(int i = 0, valid = 0; i < number_of_vehicles; i++) {
+            printf("Enter the Consumption of Fuel Per Galon For Vehicle (#%d): ", i+1);
+            valid = scanf("%d", &arr[i][0]);
+            if (valid != 1) {
+                while (valid != 1) {
+                    while ((getchar()) != '\n');
+                    printf("Invalid Input! Re-enter Value for the Consumption of Fuel Per Galon For Vehicle (#%d): ", i+1);
+                    valid = scanf("%d", &arr[i][0]);
+                }
+            }
+        }
+        for(int i = 0, valid = 0; i < number_of_vehicles; i++) {
+            printf("Enter the Miles Driven in 12 Months For Vehicle (#%d): ", i+1);
+            valid = scanf("%d", &arr[i][1]);
+            if (valid != 1) {
+                while (valid != 1) {
+                    while ((getchar()) != '\n');
+                    printf("Invalid Input! Re-enter Value for the Miles Drived in 12 Months For Vehicle (#%d): ", i+1);
+                    valid = scanf("%d", &arr[i][1]);
+                }
+            }
+        }
+    }
+
+    for(int i = 0, j = 0; i < number_of_vehicles; i++, j = 0)
+        product[i] = arr[i][j] * arr[i][j+1];
+
+    return SumArray(number_of_vehicles, product);
+}
--- main.c
+++ main.c
@@ -6,24 +6,65 @@
 #include"CGLib/src/cglib.h"
 #include"CGLib/src/cglib-other.h"
 
+int SumArray(int n, int arr[]);
+int DataValidationInteger(char str[], int  variable);
+int yes_no(char variable);
+int VehicleIO(int arr[][2], int product[], int number_of_vehicles);
+void EnergyInput(int energy_type[]);
+
 int main(void) {
 
-    int total_energy_units, total_gas_units;
-    int electricity_units[12], gas_units[12];
+// Variables to be used for Calculations:
+    int total_electricity_units, total_gas_units, vehicle_total, flights;
+// Calculate and print only if above values are not zero.
 
+// Variables for Storing Data (Not to be used for Calcultions)
+
+    int number_of_vehicles, vehicle_total, flights_y_n;
+    int electricity_units[12], gas_units[12];
 
     printf("\nCARBON FOOTPRINT CALCULATOR\n");
 
+// HouseHold Energy Input
+
     printf("\nHOUSEHOLD ENERGY:\n");
+
+// Electricity Input
 
     printf("\n1. Electricity:\nEnter the units consumed in past 12 months given in your Electricity Bill (in kWh):\n");
     EnergyInput(electricity_units);
-    total_energy_units = SumArray(12, electricity_units);
-    system('cls');
+    total_electricity_units = SumArray(12, electricity_units);
+
+// Gas Input
 
     printf("\n2. Gas:\nEnter the units consumed in past 12 months given in your Gas Bill (in Cubic Metre):\n");
     EnergyInput(gas_units);
-    total_energy_units = SumArray(12, gas_units);
+    total_gas_units = SumArray(12, gas_units);
+
+// Transportation Input
+
+    printf("\nTRANSPORTATION:\n");
+
+// Input for Vehicles
+
+    printf("How many Vehicles do you own?: ");
+    number_of_vehicles = DataValidationInteger("Number of Vehicles", number_of_vehicles);
+    
+    int vehicles[number_of_vehicles][2], total_miles_driven_all_cars[number_of_vehicles];
+    vehicle_total = VehicleIO(vehicles, total_miles_driven_all_cars, number_of_vehicles);
+    if (vehicle_total == 0)
+        printf("Skipping Over Vehicle Calculation.");
+
+// Input for Flights
+
+    printf("\nDid You Take Any Flights In the Past 12 Months? (y/n): ");
+    scanf("%c", &flights_y_n);
+
+    if (yes_no(flights_y_n) == 1) {
+        printf("\nEnter Number of Flights Taken: ");
+        DataValidationInteger("Number of Flights Taken", flights);
+    }
+    else printf("Skipping Over Flight Calculation.");
 
     return 0;
 }
