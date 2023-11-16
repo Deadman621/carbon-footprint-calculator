0d55ebac8d1831e84048104b86185566290d93ef
Muhammad Saad Bin Khalid <saadkhalid1604@gmail.com>
2023-11-16 23:22:40 +0500

    On branch Saad  Your branch is up to date with 'origin/Saad'.
     Changes to be committed:
    	modified:   Calc.c
    	modified:   Calc.exe
    	new file:   Data Base File.txt (Stores previous total here)

--- Calc.c
+++ Calc.c
@@ -5,19 +5,21 @@ float HouseholdEmission(int total_electricity_unites, int total_gas_unites){
     float Gtokwh = 11.2 ;  //cubic meter (of gas) to kwh
     float Grate = 0.203;   //Co2 released (in kg) per kwh
 
-    return ((total_electricity_unites * Erate) + ((total_gas_unites*Gtokwh) * Grate));
+    float electricity_emission = total_electricity_unites * Erate;
+    float gas_emission = (total_gas_unites * Gtokwh) * Grate;
 
+    return (electricity_emission + gas_emission);
 }
-
 
 float TransportEnergy(float vehicle_total, int flight){
     float Vrate = 10.9;   //Co2 released (in kg) per gallons
     float Frate = 250.0;   //Co2 released (in kg) per hours of flight
 
-    return ((vehicle_total * Vrate) + (flight * Frate));
+    float vehicle_emission = vehicle_total * Vrate;
+    float flight_emission = flight * Frate;
 
+    return (vehicle_emission + flight_emission);
 }
-
 
 float Foodemission(float organic_food_percentage, float inorganic_food_percentage, float locally_produced_food){
     float avgConsumption = 75.0; //Food consumed in kg in a month
@@ -29,17 +31,120 @@ float Foodemission(float organic_food_percentage, float inorganic_food_percentag
     float InorganicConsumption = avgConsumption * inorganic_food_percentage / 100;
     float LocalConsumption = avgConsumption * locally_produced_food / 100;
 
-    return ((OrganicConsumption * Orate)+(InorganicConsumption * Irate)+(LocalConsumption * Lrate));
+    float organic_emission = OrganicConsumption * Orate;
+    float inorganic_emission = InorganicConsumption * Irate;
+    float local_emission = LocalConsumption * Lrate;
 
+    return (organic_emission + inorganic_emission + local_emission);
+}
+
+
+void store(float Total){
+    
+
+    //FILE *fptr = fopen("Data Base File.txt","w");
+
+    FILE *fptr = fopen("Data Base File.txt","a+");
+    int count = 1;
+
+    if (fptr == NULL) {
+        fptr = fopen("Data Base File.txt","w+");
+    }
+
+
+    fprintf(fptr,"%f\n",Total);
+    //fprintf(fptr,"%s","\n");
+
+    fclose(fptr);
+}
+
+// QtyRequested means number of previous total requested, 
+// If number of previous total stored < QtyRequested, 
+// then all available previous totals are given in total array 
+// and QtyRequested is updated.
+int retrive(float total[], int *QtyRequested){ 
+
+
+
+    FILE *fptr = fopen("Data Base File.txt","r");
+
+    if (fptr == NULL) {
+        return 0; // Returning 0 if file does not exist
+    }
+
+    int count = 0, i;
+    float temp;
+    int line;
+    
+    
+    do {
+        line = fscanf(fptr, "%f", &temp); // fscanf returns 1 if it successfully reads %f
+        if(line == 1) {
+            count++;
+        }
+    } while(line != EOF);
+
+
+    
+    if (count == 0) {
+        fclose(fptr);
+        return 0; // Returning 0 if file is empty
+    }
+
+    rewind(fptr);
+
+    if (count < *QtyRequested) {
+
+        for(i = 0; i < count; i++) {
+            fscanf(fptr, "%f", &total[i]);
+        }
+        *QtyRequested = count;
+
+    } else {
+
+        int start = count - *QtyRequested, i;
+
+        for(i = 0; i < start; i++) {
+            fscanf(fptr, "%f", &temp); 
+        }
+
+        for(i = 0; i < *QtyRequested; i++) {
+            fscanf(fptr, "%f", &total[i]);
+        }
+
+    }
+
+    fclose(fptr);
+    return 1;
 }
 
 
 
-// int main(){
 
-//     printf("\n%f", HouseholdEmission(5,1500));
-//     printf("\n%f", TransportEnergy(5.527,46));
-//     printf("\n%f", Foodemission(5.0527,46,6.37));
+
+// int main(){  
+    
+//     float CurrentHouseholdEmissionTotal =  HouseholdEmission(1000,1000);
+//     float CurrentTransportEnergyTotal = TransportEnergy(55.27,500);
+//     float CurrentFoodemissionTotal =  Foodemission(90,90,90);
+
+//     int QtyRequested = 23, i;                                
+//     float total[QtyRequested];  
+                    
+//     //store(CurrentHouseholdEmissionTotal+CurrentTransportEnergyTotal+CurrentFoodemissionTotal);
+
+//     retrive(total,&QtyRequested);
+
+//     for(i = 0 ; i < QtyRequested ; i++){
+//         printf(" %d : %f\n",i,total[i]); // The most recent total is stored at the largest index
+//     }
+
+//     printf("q : %d",QtyRequested);
+
+
+//     // printf("\n%f",CurrentHouseholdEmissionTotal);
+//     // printf("\n%f",CurrentTransportEnergyTotal);
+//     // printf("\n%f",CurrentFoodemissionTotal);
 
     
 // }
